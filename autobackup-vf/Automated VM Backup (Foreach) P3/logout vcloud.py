acb683333cf1323ac33698d2336a0648c1b1dc8b
AZIZOS <120977315+azizos001@users.noreply.github.com>
2025-06-13 11:49:35 +0100

    Update logout vcloud.py

--- autobackup-vf/Automated VM Backup (Foreach) P3/logout vcloud.py
+++ autobackup-vf/Automated VM Backup (Foreach) P3/logout vcloud.py
@@ -11,19 +11,15 @@ def handler(context, inputs):
         "Accept": "application/json;version=39.0",
         "Authorization": f"Bearer {vCloud_token}"
     }
-    
     logout_url = f"https://{url}/cloudapi/1.0.0/sessions/current"
     parsed_url = urllib.parse.urlparse(logout_url)
-    
     conn = http.client.HTTPSConnection(parsed_url.hostname, parsed_url.port, context=ssl._create_unverified_context(), timeout=10)
     try:
         conn.request("DELETE", parsed_url.path, headers=headers)
         response = conn.getresponse()
-        
         if response.status != 204:  # 204 No Content is expected for a successful logout
             error_msg = f"Failed to logout: {response.status} {response.reason}"
             raise Exception(error_msg)
-    
     except Exception as e:
         raise e
     finally:
